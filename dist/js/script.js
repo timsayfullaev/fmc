a72aeaf3e9da27f50d18b19a2ff3dbd250b9e4f0
Timur <tim.sayfullaev@gmail.com>
2022-02-10 00:45:03 +0500

    fmc

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,6 +18,35 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="solution__arrow solution__arrow_left"><img src="icons/rounded_rectangle_7_copy_3_285.png" alt="rounded_rectangle_7_copy_3_285"></button>',
         nextArrow: '<button type="button" class="solution__arrow solution__arrow_right"><img src="icons/rounded_rectangle_7_copy_284.png" alt="rounded_rectangle_7_copy_284"></button>',
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 576,
+            settings: {
+                arrows: false,
+                dots: true,
+                dotsClass: 'intro__dots',
+            }
+            }
+        ]
+    });
+});
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.nav'),
+    menuClose = document.querySelector('.nav__close'),
+    menuLink = document.querySelectorAll('.nav__link');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('nav_active');
+});
+
+menuClose.addEventListener('click', () => {
+    menu.classList.remove('nav_active');
+});
+
+menuLink.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('nav_active');
     });
 });
